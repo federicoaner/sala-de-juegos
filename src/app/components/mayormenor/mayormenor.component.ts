29c0012c9792e534520f6635515ed762069d2e99
federicoaner <federicoaner@gmail.com>
2022-05-05 23:53:10 -0300

    sprint 3  mayorMenor maso

--- src/app/components/mayormenor/mayormenor.component.ts
+++ src/app/components/mayormenor/mayormenor.component.ts
@@ -10,6 +10,63 @@ export class MayormenorComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    //this.comenzar();
   }
+
+  puntaje : number = 0;
+  numeroAnterior : number = 0;
+  numeroSiguiente : number = 0;
+  bandera : boolean  =false;
+  perdio:boolean=false;
+  comienzo:boolean=false;
+  mensaje:string ="";
+  numeros:number[]=[1,2,3,4,5,6,7,8,9,10];
+
+
+  comenzar() {
+    this.numeroSiguiente = Math.round(Math.random() *  this.numeros.length);
+    this.puntaje = 0;
+    this.numeroAnterior = Math.round(Math.random() * this.numeros.length);
+  
+   this.comienzo=true;
+   this.perdio=false;
+  }
+
+  btnMayor() {
+    if(this.numeroSiguiente >= this.numeroAnterior){
+      this.puntaje++;
+      this.numeroAnterior = this.numeroSiguiente;
+      this.numeroSiguiente = Math.round(Math.random() * this.numeros.length);
+    } 
+    
+    else{
+    
+     alert("Perdiste el numero secreto era Menor");
+    
+      this.comenzar();
+      this.perdio=true;
+      this.comienzo=false;
+    }
+  }
+
+  btnMenor(){
+    if(this.numeroSiguiente <= this.numeroAnterior){
+      this.puntaje++;
+      this.numeroAnterior = this.numeroSiguiente;
+      this.numeroSiguiente = Math.round(Math.random() * this.numeros.length); 
+    }
+    
+    else{
+    
+     alert("Perdiste el numero era Mayor");
+     this.comenzar();
+     this.perdio=true;
+     this.comienzo=false;
+     
+    }
+  }
+
+
+
 
 }
